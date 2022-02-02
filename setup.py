155e22a0e85b360732d807c13aca838ca5137df4
Hassan Syyid <hassansyyid@gmail.com>
2022-02-02 17:09:11 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='tap-sftp-files',
+    version='0.0.1',
+    description='hotglue tap for importing files from SFTP',
+    author='hotglue',
+    url='https://hotglue.xyz',
+    classifiers=['Programming Language :: Python :: 3 :: Only'],
+    py_modules=['tap_sftp_files'],
+    install_requires=[
+        'argparse==1.4.0',
+        'pysftp==0.2.8'
+    ],
+    entry_points='''
+        [console_scripts]
+        tap-sftp-files=tap_sftp_files:main
+    ''',
+    packages=['tap_sftp_files']
+)
---
+++ tap_sftp_files/__init__.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+import os
+import json
+import argparse
+import logging
+
+from pathlib import Path
+import pysftp
+
+logger = logging.getLogger("tap-sftp-files")
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+def load_json(path):
+    with open(path) as f:
+        return json.load(f)
+
+
+def parse_args():
+    '''Parse standard command-line args.
+    Parses the command-line arguments mentioned in the SPEC and the
+    BEST_PRACTICES documents:
+    -c,--config     Config file
+    -s,--state      State file
+    -d,--discover   Run in discover mode
+    -p,--properties Properties file: DEPRECATED, please use --catalog instead
+    --catalog       Catalog file
+    Returns the parsed args object from argparse. For each argument that
+    point to JSON files (config, state, properties), we will automatically
+    load and parse the JSON file.
+    '''
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        '-c', '--config',
+        help='Config file',
+        required=True)
+
+    args = parser.parse_args()
+    if args.config:
+        setattr(args, 'config_path', args.config)
+        args.config = load_json(args.config)
+
+    return args
+
+
+def download(args):
+    logger.debug(f"Downloading data...")
+    config = args.config
+    host = config['host']
+    port = config.get('port', "")
+    remote_path = config['path_prefix']
+    target_dir = config['target_dir']
+
+    connection_config = {
+        'username': config['username'],
+        'password': config['password']
+    }
+
+    if port:
+        connection_config['port'] = port
+
+    # Establish connection to SFTP server
+    with pysftp.Connection(host, **connection_config) as sftp:
+        logger.info(f"Downloading: data from {remote_path} -> {target_dir}")
+        # Copy all files in remote_path to target_dir
+        sftp.get_d(remote_path, target_dir)
+
+    logger.info(f"Data downloaded.")
+
+
+def main():
+    # Parse command line arguments
+    args = parse_args()
+
+    # Download the data
+    download(args)
+
+
+if __name__ == "__main__":
+    main()
