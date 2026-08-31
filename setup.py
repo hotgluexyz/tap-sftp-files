be5283841a3a440aa29f0c4ff97924d14eb78db5
Lior Ben-David <liorbd@Outlook.com>
2026-08-31 21:22:20 +0200

    HGI-11051 | Add Python 3.14 compatible Paramiko dependency (#12)

--- setup.py
+++ setup.py
@@ -8,11 +8,19 @@ setup(
     description='hotglue tap for importing files from SFTP',
     author='hotglue',
     url='https://hotglue.xyz',
-    classifiers=['Programming Language :: Python :: 3 :: Only'],
+    classifiers=[
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.14',
+    ],
+    python_requires='>=3.7',
     py_modules=['tap_sftp_files'],
     install_requires=[
         'backoff==2.2.1',
-        'paramiko==3.5.1'
+        'paramiko>=2.12,<3.5; python_version < "3.8"',
+        'paramiko>=3.5.1,<4; python_version >= "3.8" and python_version < "3.9"',
+        'paramiko>=4,<5; python_version >= "3.9"'
     ],
     entry_points='''
         [console_scripts]
